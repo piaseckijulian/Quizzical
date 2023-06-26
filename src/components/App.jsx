04f53afc468b7647f8ead67ef668c04041bb085a
Julian Piasecki <julian.piasecki@outlook.com>
2023-06-27 01:30:27 +0200

    Code Splitting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,32 @@
-import React, { useState, lazy } from 'react';
+import React, { useState, lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-const Trivia = lazy(() => import('../routes/Trivia'));
 const Welcome = lazy(() => import('../routes/Welcome'));
+const Trivia = lazy(() => import('../routes/Trivia'));
 
 const App = () => {
 	const [selectedCategory, setSelectedCategory] = useState(0);
 
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route
-					path='/'
-					element={
-						<Welcome
-							setSelectedCategory={setSelectedCategory}
-							selectedCategory={selectedCategory}
-						/>
-					}
-				/>
-				<Route
-					path='/trivia'
-					element={<Trivia selectedCategory={selectedCategory} />}
-				/>
-			</Routes>
-		</BrowserRouter>
+		<Suspense>
+			<BrowserRouter>
+				<Routes>
+					<Route
+						path='/'
+						element={
+							<Welcome
+								setSelectedCategory={setSelectedCategory}
+								selectedCategory={selectedCategory}
+							/>
+						}
+					/>
+					<Route
+						path='/trivia'
+						element={<Trivia selectedCategory={selectedCategory} />}
+					/>
+				</Routes>
+			</BrowserRouter>
+		</Suspense>
 	);
 };
 
