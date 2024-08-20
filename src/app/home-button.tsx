50860d1ac79989df6725b62d858804126950270b
Julian Piasecki <julian.piasecki@outlook.com>
2024-08-21 00:47:48 +0200

    fix: reset quiz on home button click

--- src/app/home-button.tsx
+++ src/app/home-button.tsx
@@ -2,11 +2,18 @@
 
 import { Button } from "@/components/button"
 import { useCategoryStore } from "@/stores/categoryStore"
+import { useQuizStore } from "@/stores/quizStore"
 
 export const HomeButton = () => {
   const { selectedCategory } = useCategoryStore()
+  const { resetQuiz } = useQuizStore()
 
   return (
-    <Button href={`/quiz?category=${selectedCategory || 0}`}>Start quiz</Button>
+    <Button
+      href={`/quiz?category=${selectedCategory || 0}`}
+      onClick={resetQuiz}
+    >
+      Start quiz
+    </Button>
   )
 }
